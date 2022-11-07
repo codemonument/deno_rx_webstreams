8f01dc991c9ee5ea3c3ab7864708a04b876504c7
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-07 23:57:04 +0100

    add simpleTimerSource tests

---
+++ lib/sources/simpleTimerSource.test.ts
@@ -0,0 +1,52 @@
+import { assert, describe, it } from "@deps/std_testing.ts";
+import { simpleTimerSource } from "@mod";
+
+describe(`simpleTimerStream`, () => {
+  it(`should output 5 events, 1 each second (defaults)`, async () => {
+    const eventLog = [];
+    for await (const event of simpleTimerSource()) {
+      console.log(event);
+      eventLog.push(event);
+    }
+
+    assert(eventLog.length === 5);
+  });
+
+  it(`should output 5 events, 1 each 500ms`, async () => {
+    const eventLog = [];
+
+    const timer = simpleTimerSource({
+      intervalInMilliseconds: 500,
+      maxEventCount: 5,
+    });
+
+    for await (const event of timer) {
+      console.log(event);
+      eventLog.push(event);
+    }
+
+    assert(eventLog.length === 5);
+  });
+
+  it(`should be abortable by an abortController`, async () => {
+    const eventLog = [];
+    const abortController = new AbortController();
+
+    const timeoutId = setTimeout(() => {
+      abortController.abort();
+    }, 4500);
+
+    const timer = simpleTimerSource({
+      intervalInMilliseconds: 1000,
+      abortSignal: abortController.signal,
+    });
+
+    for await (const event of timer) {
+      console.log(event);
+      eventLog.push(event);
+    }
+
+    assert(eventLog.length === 5);
+    clearTimeout(timeoutId);
+  });
+});

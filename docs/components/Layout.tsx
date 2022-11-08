42f2435c8856294b95d13265b8626fb63cb181a2
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-08 03:02:42 +0100

    replace twind with openprops (twind not rendering gradients)

--- docs/components/Layout.tsx
+++ docs/components/Layout.tsx
@@ -7,17 +7,19 @@ export function Layout(props: JSX.HTMLAttributes<HTMLButtonElement>) {
       <Head>
         <title>Docs rx_webstreams</title>
         <link rel="stylesheet" href="./open-props@1.4.16/open-props.min.css" />
+        <link rel="stylesheet" href="./open-props@1.4.16/normalize.min.css" />
+        <link rel="stylesheet" href="./open-props@1.4.16/buttons.min.css" />
         <link rel="stylesheet" href="./global.css"></link>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
+      <div class="layout">
         <img
           src="/logo.svg"
           class="w-32 h-32"
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
         <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
+          Welcome to `fresh`. Try updating this message in the
+          ./routes/index.tsx file, and refresh.
         </p>
         {props.children}
       </div>

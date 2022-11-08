73fbbedcdbf63ca98512597379e400420fc51adb
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-08 03:54:31 +0100

    fix main navigation

--- docs/components/Nav.tsx
+++ docs/components/Nav.tsx
@@ -3,7 +3,7 @@ import { JSX } from "preact";
 export function Nav(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
     <nav>
-      <a href="/">Main</a>
+      <a href="./">Main</a>
       <a href="/concepts">Concepts</a>
       <a href="/examples">Examples</a>
       <a href="/sources">Sources</a>

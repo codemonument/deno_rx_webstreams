c4dadc0b1ff557c7279a3322fc137813efa87d97
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-08 02:50:29 +0100

    integrate Layout into index.tsx

---
+++ docs/components/Layout.tsx
@@ -0,0 +1,26 @@
+import { JSX } from "preact";
+import { Head } from "$fresh/runtime.ts";
+
+export function Layout(props: JSX.HTMLAttributes<HTMLButtonElement>) {
+  return (
+    <>
+      <Head>
+        <title>Docs rx_webstreams</title>
+        <link rel="stylesheet" href="./open-props@1.4.16/open-props.min.css" />
+        <link rel="stylesheet" href="./global.css"></link>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <img
+          src="/logo.svg"
+          class="w-32 h-32"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+        <p class="my-6">
+          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
+          file, and refresh.
+        </p>
+        {props.children}
+      </div>
+    </>
+  );
+}

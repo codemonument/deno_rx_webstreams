086c0eca4ea44b47cc574ad3ef48638394b393a4
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-08 03:42:48 +0100

    upgrade open-props to 1.4.21 and add colors-hsl

--- docs/components/Layout.tsx
+++ docs/components/Layout.tsx
@@ -7,9 +7,10 @@ export function Layout(props: JSX.HTMLAttributes<HTMLButtonElement>) {
     <>
       <Head>
         <title>Docs rx_webstreams</title>
-        <link rel="stylesheet" href="./open-props@1.4.16/open-props.min.css" />
-        <link rel="stylesheet" href="./open-props@1.4.16/normalize.min.css" />
-        <link rel="stylesheet" href="./open-props@1.4.16/buttons.min.css" />
+        <link rel="stylesheet" href="./open-props@1.4.21/open-props.min.css" />
+        <link rel="stylesheet" href="./open-props@1.4.21/colors-hsl.min.css" />
+        <link rel="stylesheet" href="./open-props@1.4.21/normalize.min.css" />
+        <link rel="stylesheet" href="./open-props@1.4.21/buttons.min.css" />
         <link rel="stylesheet" href="./global.css"></link>
       </Head>
 

499372d097999118140747a729d808fed9fca3ce
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-10 23:10:24 +0100

    check if uint8ToUtf8 decoder can also read some random encoding, like Win1252 => Spoiler: yes it can!

--- lib/transforms/uint8ToUtf8.test.ts
+++ lib/transforms/uint8ToUtf8.test.ts
@@ -3,9 +3,19 @@ import { fileSource, simpleCallbackTarget } from "@mod";
 import { uint8ToUtf8 } from "@mod";
 
 describe(`FileSource`, () => {
-  it(`Should transcode file bytes to text`, async () => {
+  it(`Should transcode utf8 file bytes to text`, async () => {
     const stream = await fileSource(
       `tests/testdata/fileSourceUtf8.example.txt`,
+    );
+    await stream.pipeThrough(uint8ToUtf8({}))
+      .pipeTo(
+        simpleCallbackTarget((chunk) => console.log(`Chunk: `, chunk)),
+      );
+  });
+
+  it(`Should transcode windows1252 file bytes to text`, async () => {
+    const stream = await fileSource(
+      `tests/testdata/fileSourceWin1252.example.txt`,
     );
     await stream.pipeThrough(uint8ToUtf8({}))
       .pipeTo(

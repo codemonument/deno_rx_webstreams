21ae5f16ceab15ee68cd2da68c87a212b99b238b
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-11 23:18:37 +0100

    extend mod.test

--- tests/mod.test.ts
+++ tests/mod.test.ts
@@ -1,9 +1,26 @@
 import { assertExists, describe, it } from "@deps/std_testing.ts";
-import { simpleCallbackTarget, timerSource } from "@mod";
+import {
+  bytesToString,
+  emittableSource,
+  fileSource,
+  simpleCallbackTarget,
+  stringToLines,
+  timerSource,
+} from "@mod";
 
 describe(`mod.ts`, () => {
   it(`should export correct objects and types`, () => {
+    // Sources
+    assertExists(fileSource);
     assertExists(timerSource);
+    assertExists(emittableSource);
+
+    // Transforms
+
+    assertExists(bytesToString);
+    assertExists(stringToLines);
+
+    // Targets
     assertExists(simpleCallbackTarget);
   });
 });

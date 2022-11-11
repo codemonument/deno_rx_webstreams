d9a3b27b246dfd2223adbc584179d419b437ea5f
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-11 22:45:10 +0100

    add EmittableSource + test

---
+++ lib/sources/EmittableSource.test.ts
@@ -0,0 +1,12 @@
+import { assert, describe, it } from "@deps/std_testing.ts";
+import { EmittableSource, simpleCallbackTarget } from "@mod";
+describe(`EmittableSource`, () => {
+  it(`Should create source object`, () => {
+    const stream = EmittableSource.create();
+    stream.emit(`My first event!`);
+    stream.emit(`My second event!`);
+    stream.pipeTo(simpleCallbackTarget((chunk) => console.log(chunk)));
+
+    stream.finish();
+  });
+});

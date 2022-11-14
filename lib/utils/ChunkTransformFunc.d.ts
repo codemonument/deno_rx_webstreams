6825d9d5084f97934c668cbd623a43477b49f629
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-14 10:29:34 +0100

    delete old ChunkTransformFunc and Illegalccess.error in lib/utils

--- lib/utils/ChunkTransformFunc.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * A pure function which gets a chunk in one type and returns it in another
- */
-export type ChunkTransformFunc<P, R> = (chunk: P) => R;
--- lib/utils/IllegalAccess.error.ts
+++
@@ -1,5 +0,0 @@
-export class IllegalAccess extends Error {
-  constructor(message: string) {
-    super(message);
-  }
-}

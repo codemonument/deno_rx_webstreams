e0b002b6a0fbe38f07486afc53a198515be6b444
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-14 15:59:11 +0100

    add test to map.test when validator fails

--- lib/transforms/map.test.ts
+++ lib/transforms/map.test.ts
@@ -1,4 +1,4 @@
-import { assert, describe, it } from "@deps/std_testing.ts";
+import { assert, assertRejects, describe, it } from "@deps/std_testing.ts";
 import { map, simpleCallbackTarget, timerSource } from "@mod";
 import { z } from "../../deps/zod.ts";
 
@@ -37,5 +37,27 @@ describe(`map Transform`, () => {
           console.log(`Chunk: `, chunk);
         }),
       );
+  });
+
+  it(`Should throw when validator fails`, async () => {
+    const timer = timerSource({
+      intervalInMilliseconds: 50,
+      maxEventCount: 2,
+    });
+
+    const promise = timer
+      // force-map to any, to be able to test the error emitted by the broken validator
+      // (inputting a number into the map transformer but expecting a string via zod )
+      .pipeThrough(map<number, any>((timerCount) => timerCount))
+      .pipeThrough(
+        map((timerCount) => `Event No. ${timerCount}`, {
+          validator: z.string(),
+        }),
+      )
+      .pipeTo(
+        simpleCallbackTarget((chunk) => console.log(`Chunk: `, chunk)),
+      );
+
+    await assertRejects(() => promise);
   });
 });

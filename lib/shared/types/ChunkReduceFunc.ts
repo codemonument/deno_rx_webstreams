3183ae9b9157754182b0c046809e330509d41342
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-02-21 16:23:48 +0100

    name the return value of ChunkReduceFunc Aggregate

--- lib/shared/types/ChunkReduceFunc.ts
+++ lib/shared/types/ChunkReduceFunc.ts
@@ -1,10 +1,10 @@
 import { z } from "../../../deps/zod.ts";
 
 /**
- * A function which gets a chunk in one type + the return value of the previous execution (previousState) and returns a new state
+ * A function which gets a chunk in one type + the return value of the previous execution (aggregate) and returns a new aggregate
  */
 
 /**
  * Manual TypeDef
  */
-export type ChunkReduceFunc<C, A> = (chunk: C, previousState: A) => A;
+export type ChunkReduceFunc<C, A> = (chunk: C, aggregate: A) => A;

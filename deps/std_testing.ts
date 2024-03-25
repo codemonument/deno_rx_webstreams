72df743e99164199cd823928a6ca7b933416489c
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-03-25 13:58:51 +0100

    swtich to non-deprecated assert library

--- deps/std_testing.ts
+++ deps/std_testing.ts
@@ -1,3 +1,3 @@
+export * from "https://deno.land/std@0.220.1/assert/mod.ts";
 export * from "https://deno.land/std@0.220.1/testing/bdd.ts";
-export * from "https://deno.land/std@0.220.1/testing/asserts.ts";
 export * from "https://deno.land/std@0.220.1/testing/snapshot.ts";

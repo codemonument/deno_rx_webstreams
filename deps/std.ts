3bb6be27628c23b33e8e03866096e62e88ab7447
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-03-25 16:23:57 +0100

    refactor std dependencies

---
+++ deps/std.ts
@@ -0,0 +1,2 @@
+export * from "https://deno.land/std@0.220.1/io/mod.ts";
+export * as log from "https://deno.land/std@0.220.1/log/mod.ts";
--- deps/std_log.ts
+++
@@ -1,1 +0,0 @@
-export * as log from "https://deno.land/std@0.220.1/log/mod.ts";

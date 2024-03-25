f262ced971e6658d0a93af1ee9ac542ddd09769f
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-03-25 14:36:32 +0100

    add link to TextDecoderStream

--- lib/transforms/bytesToString.ts
+++ lib/transforms/bytesToString.ts
@@ -9,7 +9,7 @@ const defaults: BytesToStringOptions = {};
  * Creates a TransformStream (Webstream) which transforms uint8Array chunks to a string.
  * Detects input encoding automatically!
  *
- * Basically the same as TextDecoderStream from Whatwg Standard
+ * Basically the same as [TextDecoderStream](https://developer.mozilla.org/en-US/docs/Web/API/TextDecoderStream) from Whatwg Standard
  */
 export function bytesToString(
   options?: BytesToStringOptions,

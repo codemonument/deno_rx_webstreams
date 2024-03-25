c9dba489a368366d1f7d7d49bdf8b01dc6b7ada1
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-03-25 15:52:07 +0100

    fix UnderlyingSourceWithController typing by including lib.dom

--- lib/utils/UnderlyingSourceWithController.ts
+++ lib/utils/UnderlyingSourceWithController.ts
@@ -4,19 +4,19 @@ import { IllegalAccess } from "../errors.ts";
  * Type R = The type which is emitted by this underlying source
  */
 export class UnderlyingSourceWithController<R = unknown>
-  implements UnderlyingSource<R> {
+  implements UnderlyingDefaultSource<R> {
   #controller?: ReadableStreamDefaultController<R>;
 
-  constructor() {
-  }
-  cancel?: ReadableStreamErrorCallback = (_reason?: unknown) => {
+  cancel(_reason?: unknown) {
     // Does not work bc. stream is mostly in use when it should be canceled!
     // this.#controller?.close();
-  };
-  pull?: ReadableStreamDefaultControllerCallback<R>;
-  start?: ReadableStreamDefaultControllerCallback<R> = (controller) => {
+  }
+
+  // pull() {}
+
+  start(controller: ReadableStreamDefaultController<R>) {
     this.#controller = controller;
-  };
+  }
   type?: undefined;
 
   get controller() {

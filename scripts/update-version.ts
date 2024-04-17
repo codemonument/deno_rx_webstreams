dce9d77b8094c797dad1388b110afe802b2cf3c3
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-04-17 17:04:15 +0200

    add script to update the version in version.ts and deno.jsonc

---
+++ scripts/update-version.ts
@@ -0,0 +1,37 @@
+import { updateConfig } from "@codemonument/update-denoconfig";
+import { ZodSemver } from "@codemonument/zod-semver";
+
+// Stage: Validate script input
+const newVersionParse = ZodSemver.safeParse(Deno.args[0]);
+
+if (!newVersionParse.success) {
+  console.error(
+    "Please provide a new semantic version number as the first positional param of this script",
+  );
+  Deno.exit(1);
+}
+
+const newVersion = newVersionParse.data;
+
+// Stage: Input valid
+// ------------------------------------------------
+console.log(`Updating version of this package to ${newVersion}`);
+
+// STAGE: Update the version in the deno.jsonc file
+// ------------------------------------------------
+const updateObject = {
+  version: newVersion,
+};
+// Note: The path is relative to the current working directory
+await updateConfig("./deno.jsonc", updateObject);
+
+// STAGE: Update the version in the version.ts file
+// ------------------------------------------------
+import { Project } from "npm:ts-morph";
+const project = new Project();
+const versionFile = project.addSourceFileAtPath("version.ts");
+
+const VERSION_variable = versionFile.getVariableDeclarationOrThrow("VERSION");
+VERSION_variable.setInitializer(`"${newVersion}"`);
+
+await project.save();
